67080b4c4b7534053e3ff6afc7cf1b07a4bfbc15
Amir Sarfraz <amirsarfraz50@gmail.com>
2026-09-09 18:15:18 +0500

    Revert "patientRoutes completed"
    This reverts commit 929408a0f7b22ba3d6eb45be8cf3169536039f00.

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 const departmentRoutes = require("./routes/departmentRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const doctorRoutes = require("./routes/doctorRoutes");
-const patientRoutes = require("./routes/patientRoutes");
 
 const app = express();
 
@@ -22,7 +21,6 @@ app.get("/", (req, res) => {
 app.use("/api/departments", departmentRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/doctors", doctorRoutes);
-app.use("/api/patients", patientRoutes);
 
 const PORT = process.env.PORT || 5000;
 
